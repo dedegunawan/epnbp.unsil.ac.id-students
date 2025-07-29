21f78fad4b3ba83cc3bc58af76a8b4af224c23b5
Dede Gunawan <dede@unsil.ac.id>
2025-07-29 19:28:44 +0700

    Update Student url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 // Gunakan import.meta.env agar bisa akses variabel environment dari Vite
-const baseURL = `${import.meta.env.VITE_BASE_URL}/api`
+const baseURL = joinUrl(import.meta.env.VITE_BASE_URL, '/api')
+
+function joinUrl(base: string, path: string): string {
+    return `${base.replace(/\/+$/, '')}/${path.replace(/^\/+/, '')}`;
+}
+
 
 export const api = axios.create({
     baseURL,
