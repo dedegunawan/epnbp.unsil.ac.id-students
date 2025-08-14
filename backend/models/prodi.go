1de8b2e028bd14831ddc7efa37d656f3643d44dd
Dede Gunawan <dede@unsil.ac.id>
2025-08-14 15:15:24 +0700

    Update Data

--- backend/models/prodi.go
+++ backend/models/prodi.go
@@ -2,19 +2,16 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type ProdiPnbp struct {
-	ID         uint           `gorm:"primaryKey"`
-	KodeProdi  string         `gorm:"column:kode_prodi;size:50;not null"`
-	NamaProdi  string         `gorm:"column:nama_prodi;size:191;not null"`
-	FakultasID uint           `gorm:"column:fakultas_id;index"`
-	Fakultas   *Fakultas      `gorm:"foreignKey:FakultasID;references:ID"` // Relasi belongsTo ke Fakultas
-	CreatedAt  time.Time      `gorm:"column:created_at"`
-	UpdatedAt  time.Time      `gorm:"column:updated_at"`
-	DeletedAt  gorm.DeletedAt `gorm:"column:deleted_at;index"`
+	ID         uint      `gorm:"primaryKey"`
+	KodeProdi  string    `gorm:"column:kode_prodi;size:50;not null"`
+	NamaProdi  string    `gorm:"column:nama_prodi;size:191;not null"`
+	FakultasID uint      `gorm:"column:fakultas_id;index"`
+	Fakultas   *Fakultas `gorm:"foreignKey:FakultasID;references:ID"` // Relasi belongsTo ke Fakultas
+	CreatedAt  time.Time `gorm:"column:created_at"`
+	UpdatedAt  time.Time `gorm:"column:updated_at"`
 }
 
 func (ProdiPnbp) TableName() string {
@@ -22,10 +19,9 @@ func (ProdiPnbp) TableName() string {
 }
 
 type FakultasPnbp struct {
-	ID           uint           `gorm:"primaryKey"`
-	KodeFakultas string         `gorm:"column:kode_fakultas;size:50;not null"`
-	NamaFakultas string         `gorm:"column:nama_fakultas;size:191;not null"`
-	CreatedAt    time.Time      `gorm:"column:created_at"`
-	UpdatedAt    time.Time      `gorm:"column:updated_at"`
-	DeletedAt    gorm.DeletedAt `gorm:"column:deleted_at;index"`
+	ID           uint      `gorm:"primaryKey"`
+	KodeFakultas string    `gorm:"column:kode_fakultas;size:50;not null"`
+	NamaFakultas string    `gorm:"column:nama_fakultas;size:191;not null"`
+	CreatedAt    time.Time `gorm:"column:created_at"`
+	UpdatedAt    time.Time `gorm:"column:updated_at"`
 }
